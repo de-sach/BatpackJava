e8e19304a79108e5c31440158e9f7f9fa36c377a
sach <p.sacharias@gmail.com>
2017-01-23 21:55:05 +0100

    started to get things running, is fun yes?

--- src/batterymonitorsystem/FXMLDocumentController.java
+++ src/batterymonitorsystem/FXMLDocumentController.java
@@ -18,13 +18,19 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
+//    @FXML
+//    private Label label;
+//    
+//    @FXML
+//    private void handleButtonAction(ActionEvent event) {
+//        System.out.println("You clicked me!");
+//        label.setText("Hello World!");
+//    }
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void exit(ActionEvent event){
+        System.out.println("exiting");
+        System.exit(0);
+        
     }
     
     @Override
---
+++ src/batterymonitorsystem/batteryMonitorLayoutController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package batterymonitorsystem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author sach
+ */
+public class batteryMonitorLayoutController implements Initializable {
+    @FXML
+    private void exit(ActionEvent event){
+        System.out.println("exiting");
+        System.exit(0);
+    }
+    
+    @FXML
+    private void showAbout(ActionEvent event){
+        System.out.println("this is some software thingy");
+    }
+    
+    @FXML
+    private void showConnected(ActionEvent e){
+        System.out.println("is it connected?");
+        //TODO
+    }
+    
+    @FXML
+    private void connect(ActionEvent event){
+        try {
+            System.out.println("connecting");
+            //TODO
+            //connectToBatterypack(); 
+        } catch (Exception e) {
+            System.out.println("not connected error");
+        }
+    }
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
