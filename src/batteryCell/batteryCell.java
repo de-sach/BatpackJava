1e6d37ce88e26a0154449dcb0668bd5038c1db55
sach <p.sacharias@gmail.com>
2017-01-30 09:49:11 +0100

    added Battery cell

---
+++ src/batteryCell/batteryCell.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package batteryCell;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author sach
+ */
+public class batteryCell {
+    private Double temperature;
+    private Double voltage;
+    private int id;
+    private int health;
+    private List<Double> temperatureProgress;
+    private List<Double> voltageProgress;
+    private final Instant creation;
+    private Instant lastMeasurement;
+    
+    
+    public batteryCell(double temp, double volt, int id, int health){
+        this.temperature = temp;
+        this.voltage = volt;
+        this.id = id;
+        this.health = health;
+        temperatureProgress = new ArrayList<>();
+        voltageProgress = new ArrayList<>();
+        this.creation = Instant.now();
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+        this.temperatureProgress.add(temperature);
+        this.lastMeasurement = Instant.now();
+    }
+
+    public double getVoltage() {
+        return voltage;
+    }
+
+    public void setVoltage(double voltage) {
+        this.voltage = voltage;
+        this.voltageProgress.add(voltage);
+        this.lastMeasurement = Instant.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
