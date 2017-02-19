149a2deffd9764db539ad744ca2cf1b2ed7c799e
sach <p.sacharias@gmail.com>
2017-02-19 15:31:52 +0100

    added comm port library & changed the library path for the virtual machine to also load libraries from windows (person specific)

---
+++ src/communication/PortMonitor.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2017 Peter
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package communication;
+
+import gnu.io.CommPortIdentifier;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+/**
+ *
+ * @author Peter
+ */
+public class PortMonitor {
+    List<SerialPort> serialPorts;
+    public PortMonitor(){
+        serialPorts=new ArrayList<>();
+        this.addAllSerialPorts();
+    }
+    
+    private void addAllSerialPorts(){
+        Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
+        while(portEnum.hasMoreElements()){
+            System.out.println("port found");
+            CommPortIdentifier portId = portEnum.nextElement();
+            if(portId.getPortType()==CommPortIdentifier.PORT_SERIAL){
+                SerialPort sp = new SerialPort(portId);
+                System.out.println("new serial port added");
+            }
+        }
+    }
+}
